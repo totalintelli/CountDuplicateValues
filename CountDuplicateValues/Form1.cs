029db3f82417304886689bdcaadd11e760f78987
DEVZONEYDS\USER <kwanwoo5@naver.com>
2017-12-21 20:51:21 +0900

    first commit

---
+++ CountDuplicateValues/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CountDuplicateValues
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            double[] numbers = { 1, 1, 1, 2, 3, 3, 4, 4, 5 };
+
+
+
+            List<double> sameNumbers = numbers.ToList().FindAll(SameNumber);
+        }
+
+        public static bool SameNumber(double value1, double Value2)
+        {
+            if (value1 == Value2 )
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
